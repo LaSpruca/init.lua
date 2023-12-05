e5755729d391ef7db9bcd7a3122d09bef93c18e6
Connor  Hare <me@laspruca.nz>
2023-12-05 23:28:52 +1300

    Add a readme lol

---
+++ README.md
@@ -0,0 +1,2 @@
+My personal Neovim config. It's not the greatest, I do not reccomend it, but it
+works for me so ¯\\_(ツ)_/¯
--- lua/laspruca/formatter.lua
+++ lua/laspruca/formatter.lua
@@ -21,14 +21,6 @@ require("formatter").setup({
 		typescript = { prettier },
 		typescriptreacct = { prettier },
 		svelte = { prettier },
-
-		-- Use the special "*" filetype for defining formatter configurations on
-		-- any filetype
-		["*"] = {
-			-- "formatter.filetypes.any" defines default configurations for any
-			-- filetype
-			require("formatter.filetypes.any").remove_trailing_whitespace,
-		},
 	},
 })
 
