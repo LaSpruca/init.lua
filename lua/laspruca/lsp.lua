8b2ab7200b6cfd6de63ecad3442732ade2362187
Connor  Hare <me@laspruca.nz>
2023-12-09 08:10:45 +1300

    Add dartls to lspzero startup

--- lua/laspruca/lsp.lua
+++ lua/laspruca/lsp.lua
@@ -1,4 +1,5 @@
 local lsp_zero = require("lsp-zero")
+local lspconfig = require("lspconfig")
 
 require("mason").setup()
 require("mason-tool-installer").setup({
@@ -26,6 +27,7 @@ lsp_zero.on_attach(function(client, bufnr)
 	-- Add diagnostics keybind
 	vim.keymap.set("n", "g.", vim.lsp.buf.code_action, { buffer = bufnr })
 end)
+lspconfig.dartls.setup()
 
 require("mason-lspconfig").setup({
 	handlers = {
