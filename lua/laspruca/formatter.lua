936d2e76843ad42e11134b333fee1c1bc5483f2d
Connor  Hare <me@laspruca.nz>
2023-12-13 00:45:34 +1300

    Re add formatter.lua... oops

---
+++ lua/laspruca/formatter.lua
@@ -0,0 +1,37 @@
+local prettier = require("formatter.filetypes.javascript").prettier
+
+require("formatter").setup({
+	-- Enable or disable logging
+	logging = true,
+	-- Set the log level
+	log_level = vim.log.levels.WARN,
+	-- All formatter configurations are opt-in
+	filetype = {
+		-- Formatter configurations for filetype "lua" go here
+		-- and will be executed in order
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+
+		rust = {
+			require("formatter.filetypes.rust").rustfmt,
+		},
+		javascript = { prettier },
+		javascriptreact = { prettier },
+		typescript = { prettier },
+		typescriptreacct = { prettier },
+		svelte = { prettier },
+		cpp = { require("formatter.filetypes.cpp").clangformat },
+		c = { require("formatter.filetypes.cpp").clangformat },
+	},
+})
+
+-- Format on save
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
