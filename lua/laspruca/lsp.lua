d4d01dad578ea4ccb55334ab5177be269d9f150f
Connor Hare <me@laspruca.nz>
2024-04-09 13:55:19 +1200

    feat: remove dartls

--- lua/laspruca/lsp.lua
+++ lua/laspruca/lsp.lua
@@ -1,5 +1,5 @@
 local lsp_zero = require("lsp-zero")
-local lspconfig = require("lspconfig")
+-- local lspconfig = require("lspconfig")
 
 require("mason").setup()
 require("mason-tool-installer").setup({
@@ -30,8 +30,6 @@ lsp_zero.on_attach(function(client, bufnr)
 	-- Add diagnostics keybind
 	vim.keymap.set("n", "g.", vim.lsp.buf.code_action, { buffer = bufnr })
 end)
-
-lspconfig.dartls.setup({})
 
 require("mason-lspconfig").setup({
 	handlers = {
