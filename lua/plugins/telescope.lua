ef6b4641220d23f2cea9cb54772517934323d715
Connor  Hare <me@laspruca.nz>
2024-07-13 00:34:35 +1200

    feat: Make all folders show up in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,14 +4,32 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local telescope = require("telescope")
+		local telescopeConfig = require("telescope.config")
+
+		-- Clone the default Telescope configuration
+		local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+		-- I want to search in hidden/dot files.
+		table.insert(vimgrep_arguments, "--hidden")
+		-- I don't want to search in the `.git` directory.
+		table.insert(vimgrep_arguments, "--glob")
+		table.insert(vimgrep_arguments, "!**/.git/*")
 
 		telescope.setup({
+			defaults = {
+				-- `hidden = true` is not supported in text grep commands.
+				vimgrep_arguments = vimgrep_arguments,
+			},
 			pickers = {
 				find_files = {
 					theme = "dropdown",
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+				},
+				live_grep = {
+					theme = "dropdown",
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 				},
 			},
-
 		})
 
 		local builtin = require("telescope.builtin")
